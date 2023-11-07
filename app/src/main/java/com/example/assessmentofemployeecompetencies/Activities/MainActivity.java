4ea4e8c7f667d93bc001b3e205f688207bae3e5f
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-11-08 02:05:08 +0300

    directors' reg

--- app/src/main/java/com/example/assessmentofemployeecompetencies/Activities/MainActivity.java
+++ app/src/main/java/com/example/assessmentofemployeecompetencies/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.assessmentofemployeecompetencies;
+package com.example.assessmentofemployeecompetencies.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.assessmentofemployeecompetencies.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
