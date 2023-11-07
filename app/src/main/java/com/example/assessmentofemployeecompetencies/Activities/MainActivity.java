cfd5d1d3ba9823a198cd7f1485043541fd2f5d99
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-11-08 02:37:47 +0300

    binding

--- app/src/main/java/com/example/assessmentofemployeecompetencies/Activities/MainActivity.java
+++ app/src/main/java/com/example/assessmentofemployeecompetencies/Activities/MainActivity.java
@@ -2,16 +2,28 @@ package com.example.assessmentofemployeecompetencies.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.assessmentofemployeecompetencies.R;
+import com.example.assessmentofemployeecompetencies.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("sf");
+            }
+        });
     }
 
 }
